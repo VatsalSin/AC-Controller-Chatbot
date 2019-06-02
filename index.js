8ebabe99341a9d29cd027f6a6cc1d65f52f7dbf1
VatsalSin <33460761+VatsalSin@users.noreply.github.com>
2019-06-02 23:09:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ assistant.intent('power_ac', conv => {
           resolve("Air Conditioner is already " + ac);
          }
          else{
-            axios.patch('https://ac-controller-25c84.firebaseio.com/ac_power.json',{ac_power}
+            axios.patch('https://ac-controller-25c84.firebaseio.com/.json',{"ac_power": ac_power}
             )
             .then(function (response) {
               console.log(response);
